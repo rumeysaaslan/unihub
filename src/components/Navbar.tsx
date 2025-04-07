ec57c071a2b6c8a0d9701e7a194c0101d611998d
Rumeysa Demir <rumeysa@Receps-MacBook-Pro.local>
2025-04-07 08:28:55 +0300

    yurt menu eklendi css sorunlari duzeltildi

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,9 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='flex justify-center gap-4 md:gap-11 p-1 mb-3.5'>
-      <Link style={{justifyContent:'center', alignItems:'center', border:'2px solid #efe8dd', borderRadius:'20px', padding:'4px'}} href={'/blog'}> <span style={{color:'#ff3131',fontFamily:'bristol'}}>Yurt Blog</span></Link>
-     <Link  style={{justifyContent:'center', alignItems:'center', border:'2px solid #efe8dd', borderRadius:'20px', padding:'4px'}}  href={"/yurtlink"}><span style={{color:'#ff3131',fontFamily:'bristol'}}>Yurt Link</span></Link>
-  
-           
+      <Link style={{ justifyContent: 'center', alignItems: 'center', border: '2px solid #efe8dd', borderRadius: '20px', padding: '4px' }} href={'/'}> <span style={{ color: '#ff3131', fontFamily: 'bristol' }}>Yurt Menü</span></Link>
+      <Link style={{ justifyContent: 'center', alignItems: 'center', border: '2px solid #efe8dd', borderRadius: '20px', padding: '4px' }} href={'/blog'}> <span style={{ color: '#ff3131', fontFamily: 'bristol' }}>Yurt Blog</span></Link>
+      <Link style={{ justifyContent: 'center', alignItems: 'center', border: '2px solid #efe8dd', borderRadius: '20px', padding: '4px' }} href={"/yurtlink"}><span style={{ color: '#ff3131', fontFamily: 'bristol' }}>Yurt Link</span></Link>
     </div>
   )
 }
