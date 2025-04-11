b60c62e8c888338cdf239934e8da49b3e036c915
Rumeysa Demir <rumeysa@Receps-MacBook-Pro.local>
2025-04-11 11:22:28 +0300

    navbar componenet eklendi

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,10 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='flex justify-center gap-4 md:gap-11 p-1 mb-3.5'>
-      <Link style={{ justifyContent: 'center', alignItems: 'center', border: '2px solid #efe8dd', borderRadius: '20px', padding: '4px' }} href={'/'}> <span style={{ color: '#ff3131', fontFamily: 'bristol' }}>Yurt Menü</span></Link>
-      <Link style={{ justifyContent: 'center', alignItems: 'center', border: '2px solid #efe8dd', borderRadius: '20px', padding: '4px' }} href={'/blog'}> <span style={{ color: '#ff3131', fontFamily: 'bristol' }}>Yurt Blog</span></Link>
-      <Link style={{ justifyContent: 'center', alignItems: 'center', border: '2px solid #efe8dd', borderRadius: '20px', padding: '4px' }} href={"/yurtlink"}><span style={{ color: '#ff3131', fontFamily: 'bristol' }}>Yurt Link</span></Link>
+      <Link style={{ justifyContent: 'center', alignItems: 'center', border: '2px solid #efe8dd', borderRadius: '20px', padding: '4px' }} href={'/'}> <span style={{ color: '#ff3131', fontFamily: 'bristol' }}>KYk Menü</span></Link>
+      <Link style={{ justifyContent: 'center', alignItems: 'center', border: '2px solid #efe8dd', borderRadius: '20px', padding: '4px' }} href={'/blog'}> <span style={{ color: '#ff3131', fontFamily: 'bristol' }}>İhtiyaçlar</span></Link>
+      <Link style={{ justifyContent: 'center', alignItems: 'center', border: '2px solid #efe8dd', borderRadius: '20px', padding: '4px' }} href={"/yurtlink"}><span style={{ color: '#ff3131', fontFamily: 'bristol' }}>Kyk Pano</span></Link>
+      <Link style={{ justifyContent: 'center', alignItems: 'center', border: '2px solid #efe8dd', borderRadius: '20px', padding: '4px' }} href={"/yurtlink"}><span style={{ color: '#ff3131', fontFamily: 'bristol' }}>Profil</span></Link>
     </div>
   )
 }
