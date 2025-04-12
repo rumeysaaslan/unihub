12bb6c32757605238758c1928ef11a5787e3ed2d
Rumeysa Demir <rumeysa@Receps-MacBook-Pro.local>
2025-04-12 11:54:33 +0300

    web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/react"
 
 import Header from "@/components/header";
 
@@ -45,6 +46,8 @@ export default function RootLayout({
             {children}
           </main>
         </ThemeProvider>
+        <Analytics />
+
       </body>
     </html>
   );
