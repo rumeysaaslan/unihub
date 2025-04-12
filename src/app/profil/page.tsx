038780676ec56c003c7685e96d6cf373252bb321
Rumeysa Demir <rumeysa@Receps-MacBook-Pro.local>
2025-04-12 19:59:45 +0300

    link isimleri duzeltildi

--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -3,7 +3,20 @@ import React from 'react'
 function Profile() {
   return (
     <div style={{display:"flex", flexDirection:"row", backgroundColor:'#efe8dd' }} className='space-x-30 justify-center  h-screen'>
-  
+    <div className='flex justify-center  '>
+      <ol>- Profil-
+       
+      </ol>
+    </div>
+
+    <div>
+    <div className='flex justify-center items-center '>
+      <ol>- Bildirimler -
+       
+      </ol>
+    </div>
+
+    </div>
     
     </div>
   )
